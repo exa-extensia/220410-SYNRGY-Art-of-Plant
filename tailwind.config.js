e9c5d215ed6ed8b278fcbbf1c6fefb9e8ab8b292
exaextensia <exa.extensia@gmail.com>
2022-05-10 16:34:49 +0700

    feat: push all to GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	content: ["./*.{html,js}"],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {
+				"neon-pink": "#fe6dfe",
+				"neon-blue": "#00e5ea",
+				"neon-green": "#b3f857",
+				"neon-yellow": "#ffd62e",
+				"neon-red": "#f14264",
+				red: "#e24300",
+			},
+		},
+	},
+	plugins: [],
+};
